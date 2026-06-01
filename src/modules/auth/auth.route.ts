61954eeff92634309c7e154bf94e18f196751ca0
doeun <doroming99@gmail.com>
2026-06-01 11:50:55 +0900

    SCRUM-49 feat : 구글 소셜 로그인 구현

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -1,12 +1,15 @@
 import { type Router as RouterType, Router } from 'express';
 import { validate } from '@/common/middleware/validate.middleware';
-import { kakaoLoginController, logoutController,refreshController } from './auth.controller';
-import { kakaoLoginSchema, refreshTokenSchema } from './auth.schema';
+import { googleLoginController, kakaoLoginController, logoutController,refreshController } from './auth.controller';
+import { googleLoginSchema, kakaoLoginSchema, refreshTokenSchema } from './auth.schema';
 
 const router: RouterType = Router();
 
 router.post('/kakao', validate({body: kakaoLoginSchema}),
 kakaoLoginController);
+
+router.post('/google', validate({ body: googleLoginSchema }),
+googleLoginController);
 
 router.delete('/logout', validate({ body: refreshTokenSchema }), logoutController);
 
